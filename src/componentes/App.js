a50a614bf5aaaaffa71264880b1814011fd1bcdc
Erickowski <daniel_perez_03081995@hotmail.com>
2019-11-03 14:02:34 -0600

    Primer commit

---
+++ src/componentes/App.js
@@ -0,0 +1,102 @@
+import React, {Component} from 'react';
+import '../css/App.css';
+import Header from './Header';
+import Formulario from './Formulario';
+import Listado from './Listado';
+import ControlPresupuesto from './ControlPresupuesto';
+import {validarPresupuesto} from '../Helper';
+
+class App extends Component {
+  state = {
+    presupuesto: '',
+    restante: '',
+    gastos: {}
+  }
+
+  componentDidMount() {
+    this.obtenerPresupuesto();
+  }
+
+  obtenerPresupuesto = () => {
+    let presupuesto = prompt('¿Cúal es su presupuesto?');
+
+    let resultado = validarPresupuesto(presupuesto);
+
+    if(resultado){
+      this.setState({
+        presupuesto: presupuesto,
+        restante: presupuesto
+      })
+    } else{
+      //Volver a repetir la funcion
+      this.obtenerPresupuesto();
+    }
+  }
+
+  //Agregar un nuevo gasto al state
+  agregarGasto = gasto => {
+    //tomar una copia del state actual
+    const gastos = {...this.state.gastos};
+
+    //Agregar el gasto al objeto del state
+    gastos[`gasto${Date.now()}`] = gasto;
+    
+    //restamos el presupuesto
+    this.restarPresupuesto(gasto.cantidadGasto);
+
+    //ponerlo en state
+    this.setState({
+      gastos
+    })
+  }
+
+  //Restar del presupuesto cuando un gasto se crea
+  restarPresupuesto = cantidad => {
+    //leer el gasto
+    let restar = Number(cantidad);
+
+    //tomar una copia del state actual
+    let restante = this.state.restante;
+
+    //lo restamos
+    restante -= restar;
+
+    //lo convertimos a String
+    restante = String(restante);
+
+    //agregamos el nuevo state
+    this.setState({
+      restante
+    })
+  }
+
+  render(){
+    return (
+      <div className="App container">
+        <Header
+          titulo='Gasto semanal'
+        />
+        <div className='contenido-principal contenido'>
+          <div className='row'>
+            <div className='one-half column'>
+              <Formulario 
+                agregarGasto = {this.agregarGasto}
+              />
+            </div>
+            <div className='one-half column'>
+              <Listado 
+                gastos={this.state.gastos}
+              />
+              <ControlPresupuesto 
+                presupuesto = {this.state.presupuesto}
+                restante = {this.state.restante}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default App;
